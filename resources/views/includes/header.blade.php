fceff25c20da0fd7e987157ae3a2f0f4f3d7075d
shafiur <sr683@njit.edu>
2018-10-23 20:16:32 -0400

    front header changed from link

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,7 +4,7 @@
 
     <div class="navbar">
         <div class="navbar-inner">
-            <a id="logo" href="/">Shafiur Rahman IS601</a>
+            <h1>Shafiur Rahman IS601<h/>
             <ul class="nav">
                 <li><a href="{{route('home')}}">Home</a></li>
                 <li><a href="{{route('about')}}">About</a></li>
